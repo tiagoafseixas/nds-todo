ec74c1ac52ed1b54aed905f730cf7c283f22f1b9
tiagoafseixas <tiagoafseixas@gmail.com>
2017-02-13 22:24:25 +0000

    Added Actions to Todo Table

--- app/modules/TodoRow.js
+++ app/modules/TodoRow.js
@@ -1,15 +1,29 @@
 import React, { PropTypes } from 'react';
 import {Table, TableRow, TableRowColumn} from 'material-ui/Table';
+import IconButton from 'material-ui/IconButton';
+import ActionAlarm from 'material-ui/svg-icons/action/alarm';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ActionDone from 'material-ui/svg-icons/action/done';
 
 class TodoRow extends React.Component
 {
     render() {
         return(
             <TableRow className = "todo-row">
-                <TableRowColumn></TableRowColumn>
                 <TableRowColumn>{this.props.title}</TableRowColumn>
                 <TableRowColumn>{this.props.duedate} {this.props.duetime}</TableRowColumn>
                 <TableRowColumn>{this.props.description}</TableRowColumn>
+                <TableRowColumn>
+                    <IconButton tooltip="SVG Icon">
+                        <ActionAlarm />
+                    </IconButton>
+                    <IconButton tooltip="SVG Icon">
+                        <ActionDelete />
+                    </IconButton>
+                    <IconButton tooltip="SVG Icon">
+                        <ActionDone />
+                    </IconButton>
+                </TableRowColumn>
             </TableRow>
         )
     }
